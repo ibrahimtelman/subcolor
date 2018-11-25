15c835cba11d46d11d371782c829a8820b369403
İbrahim Telman <cheriorn@gmail.com>
2018-11-25 22:17:33 +0300

    color-gradient package added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import ColorGradient from "color-gradient";
 import Header from "./components/header";
 import Colors from "./components/colors";
 import "./App.css";
+
+const colors = ColorGradient("#000", "#fff", 6);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Colors />
+
+        <Colors colors={colors} />
       </div>
     );
   }
--- src/components/colors/Colors.js
+++ src/components/colors/Colors.js
@@ -1,17 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Color from "../color";
 import "./style.scss";
 
 class Colors extends React.Component {
   render() {
+    const { colors } = this.props;
     return (
-      <div className={"colors"}>
-        <Color color_code={"#342"} />
-        <Color color_code={"#c7667c"} />
-        <Color />
+      <div className={"colors-wrapper"}>
+        <div className={"colors"}>
+          {colors.map(color => (
+            <Color color_code={color} />
+          ))}
+        </div>
       </div>
     );
   }
 }
+
+Colors.prototypes = {
+  colors: PropTypes.array
+};
 
 export default Colors;
